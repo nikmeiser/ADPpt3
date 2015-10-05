7a6b7480b103c568e5982dfbb6d9e020bbc47b82
Michael Cheng <michael.zhang.cheng@gmail.com>
2015-10-05 14:16:14 -0400

    added fibbonaci with looping

---
+++ spec/fibbonaci-loop-spec.js
@@ -0,0 +1,19 @@
+describe('fibbonaci with looping', function() {
+    var fib = require('../lib/fibbonaci-loop')
+    describe('insufficient call', function() {
+        it('should return false from non number call', function() {
+            expect(fib()).toBe(false)
+            expect(fib('string')).toBe(false)
+            expect(fib({})).toBe(false)
+        })
+        it('should return false from negative number', function() {
+            expect(fib(-3)).toBe(false)
+        })
+    })
+    describe('calls with numbers', function() {
+        it('should work with numbers', function() {
+            expect(fib(1)).toEqual([1])
+            expect(fib(10)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+        })
+    })
+})
